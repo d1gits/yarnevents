fd9e1d5af84181aba24a9c28318bdc8651f38689
Kasper Meilgaard <kasper.meilgaard@leaseplan.com>
2020-02-08 15:54:34 +0100

    Updates

---
+++ precache-manifest.2ddb38ac62748c17644b3d27985f6050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b6db2909927bae20eb17c6381aca57",
+    "url": "/yarnevents/index.html"
+  },
+  {
+    "revision": "7aba7e0a815b1cbd6aef",
+    "url": "/yarnevents/static/css/2.ad110307.chunk.css"
+  },
+  {
+    "revision": "e19a1406a8abbbd79b34",
+    "url": "/yarnevents/static/css/main.ac65af22.chunk.css"
+  },
+  {
+    "revision": "7aba7e0a815b1cbd6aef",
+    "url": "/yarnevents/static/js/2.e4c21451.chunk.js"
+  },
+  {
+    "revision": "c69f90ed80d1bca09d3f2195c08f55d3",
+    "url": "/yarnevents/static/js/2.e4c21451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19a1406a8abbbd79b34",
+    "url": "/yarnevents/static/js/main.3b406109.chunk.js"
+  },
+  {
+    "revision": "0355ab801d3cfcc4673b",
+    "url": "/yarnevents/static/js/runtime-main.a8c08fa6.js"
+  }
+]);
