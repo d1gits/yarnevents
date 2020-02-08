8a3b692074a09c5624b518630276d3eacf2b37ca
Kasper Meilgaard <kasper.meilgaard@leaseplan.com>
2020-02-08 16:16:19 +0100

    https

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,9 @@ export default class DemoApp extends React.Component {
           <Navbar.Brand href="#home">Yarn Events</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="#home">Calendar</Nav.Link>
-            <Nav.Link href="#home">Map</Nav.Link>
+            <Nav.Link href="#home">Map view</Nav.Link>
           </Nav>
+
           <Nav className="ml-auto">
             <Nav.Link href="#home">Suggest an event</Nav.Link>
           </Nav>
